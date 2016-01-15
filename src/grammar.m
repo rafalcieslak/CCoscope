982c7d037a90d047f9f8542d077e64930fba2474
Rafał Cieślak <rafalcieslak256@gmail.com>
2016-01-15 21:02:32 +0100

    AST generation for expressions

--- src/grammar.m
+++ src/grammar.m
@@ -24,10 +24,12 @@
 
 %startsymbol Start EOF
 
-%attribute value_float  float
+%attribute value_float   float
 %attribute value_int     int
 %attribute id            std::string
 %attribute reason        std::string
+
+%attribute tree          std::shared_ptr<ExprAST>
 
 %constraint IDENTIFIER  id 1 2
 %constraint TYPE        id 1 2
@@ -35,9 +37,24 @@
 %constraint LITERAL_INT value_int 1 2
 %constraint LITERAL_FLOAT value_float 1 2
 
+%constraint Expr10 tree 1 2
+%constraint Expr20 tree 1 2
+%constraint Expr30 tree 1 2
+%constraint Expr40 tree 1 2
+%constraint Expr50 tree 1 2
+%constraint Expr100 tree 1 2
+%constraint Expression tree 1 2
+%constraint Return tree 1 2
+%constraint Statement tree 1 2
+
+%intokenheader #include <memory>
+%intokenheader #include "tree.h"
+
 #include <cassert>
 #define ASSERT( X ) { assert( ( X ) ); }
 #include <cstdio>
+#include "tree.h"
+#include <memory>
 
 % Start : Module
 %       ;
@@ -88,17 +105,31 @@
 % VarDef : KEYWORD_VAR IDENTIFIER COLON TYPE SEMICOLON
 %        ;
 
+
 % StatementList : Statement StatementList
+{   auto statementlistAST = std::dynamic_pointer_cast<StatementListAST>( StatementList2->tree.front() );
+    statementlistAST->Prepend(Statement1->tree.front());
+    return StatementList2;
+}
 %               |
+{   token t(tkn_StatementList);
+    t.tree.push_back(std::make_shared<StatementListAST>());
+    return t;
+}
 %               ;
+
 
 % Statement : Assignment
 %           | If
 %           | While
 %           | For
 %           | Return
+{   Return1->type = tkn_Statement;
+    return Return1;
+}
 %           | Block
 %           ;
+
 
 % Assignment : IDENTIFIER ASSIGN Expression SEMICOLON
 %            ;
@@ -110,41 +141,152 @@
 % While : KEYWORD_WHILE LPAR Expression RPAR Block
 %       ;
 
+
 % Return : KEYWORD_RETURN Expression SEMICOLON
+{   token t(tkn_Return);
+    t.tree.push_back(std::make_shared<ReturnExprAST>(Expression2->tree.front()));
+    return t;
+}
 %        ;
 
 
 % Expression  : Expr20
+{   Expr201->type = tkn_Expression;
+    return Expr201;
+}
 %             ;
 
+
 % Expr20     : Expr20 AND Expr30
+{   token t(tkn_Expr20);
+    t.tree.push_back(std::make_shared<BinaryExprAST>("LOGICAL_AND",
+        Expr201->tree.front(),
+        Expr303->tree.front() ));
+    return t;
+}
 %            | Expr20 OR  Expr30
+{   token t(tkn_Expr20);
+    t.tree.push_back(std::make_shared<BinaryExprAST>("LOGICAL_OR",
+        Expr201->tree.front(),
+        Expr303->tree.front() ));
+    return t;
+}
 %            | Expr30
+{   Expr301->type = tkn_Expr20;
+    return Expr301;
+}
 %            ;
 
 /* Note that both sides of operator == and similar are of the same LOWER precedence.
  * This forbids " x == y == z " or " a < b < c " which would be very confusing. */
 % Expr30     : Expr40 EQUAL     Expr40
+{   token t(tkn_Expr30);
+    t.tree.push_back(std::make_shared<BinaryExprAST>("EQUAL",
+        Expr401->tree.front(),
+        Expr403->tree.front() ));
+    return t;
+}
 %            | Expr40 NEQUAL    Expr40
+{   token t(tkn_Expr30);
+    t.tree.push_back(std::make_shared<BinaryExprAST>("NEQUAL",
+        Expr401->tree.front(),
+        Expr403->tree.front() ));
+    return t;
+}
 %            | Expr40 LESS      Expr40
+{   token t(tkn_Expr30);
+    t.tree.push_back(std::make_shared<BinaryExprAST>("LESS",
+        Expr401->tree.front(),
+        Expr403->tree.front() ));
+    return t;
+}
 %            | Expr40 GREATER   Expr40
+{   token t(tkn_Expr30);
+    t.tree.push_back(std::make_shared<BinaryExprAST>("GREATER",
+        Expr401->tree.front(),
+        Expr403->tree.front() ));
+    return t;
+}
 %            | Expr40 LESSEQ    Expr40
+{   token t(tkn_Expr30);
+    t.tree.push_back(std::make_shared<BinaryExprAST>("LESSEQ",
+        Expr401->tree.front(),
+        Expr403->tree.front() ));
+    return t;
+}
 %            | Expr40 GREATEREQ Expr40
+{   token t(tkn_Expr30);
+    t.tree.push_back(std::make_shared<BinaryExprAST>("GREATEREQ",
+        Expr401->tree.front(),
+        Expr403->tree.front() ));
+    return t;
+}
 %            | Expr40
+{   Expr401->type = tkn_Expr30;
+    return Expr401;
+}
 %            ;
 
 % Expr40     : Expr40 ADD Expr50
+{   token t(tkn_Expr40);
+    t.tree.push_back(std::make_shared<BinaryExprAST>("ADD",
+        Expr401->tree.front(),
+        Expr503->tree.front() ));
+    return t;
+}
 %            | Expr40 SUB Expr50
+{   token t(tkn_Expr40);
+    t.tree.push_back(std::make_shared<BinaryExprAST>("SUB",
+        Expr401->tree.front(),
+        Expr503->tree.front() ));
+    return t;
+}
 %            | Expr50
+{   Expr501->type = tkn_Expr40;
+    return Expr501;
+}
 %            ;
 
 % Expr50     : Expr50 MULT Expr100
+{   token t(tkn_Expr50);
+    t.tree.push_back(std::make_shared<BinaryExprAST>("MULT",
+        Expr501 ->tree.front(),
+        Expr1003->tree.front() ));
+    return t;
+}
 %            | Expr50 DIV  Expr100
+{   token t(tkn_Expr50);
+    t.tree.push_back(std::make_shared<BinaryExprAST>("DIV",
+        Expr501 ->tree.front(),
+        Expr1003->tree.front() ));
+    return t;
+}
 %            | Expr50 MOD  Expr100
+{   token t(tkn_Expr50);
+    t.tree.push_back(std::make_shared<BinaryExprAST>("MOD",
+        Expr501 ->tree.front(),
+        Expr1003->tree.front() ));
+    return t;
+}
 %            | Expr100
+{   Expr1001->type = tkn_Expr50;
+    return Expr1001;
+}
 %            ;
 
+
 % Expr100    : LITERAL_INT
+{   token t(tkn_Expr100);
+    t.tree.push_back(std::make_shared<NumberExprAST>(LITERAL_INT1->value_int.front()));
+    return t;
+}
 %            | IDENTIFIER
+{   token t(tkn_Expr100);
+    t.tree.push_back(std::make_shared<VariableExprAST>(IDENTIFIER1->id.front()));
+    return t;
+}
 %            | LPAR Expression RPAR
+{   Expression2->type = tkn_Expr100;
+    return Expression2;
+}
 %            ;
--- src/tree.h
+++ src/tree.h
@@ -5,6 +5,7 @@
 #include <memory>
 #include <vector>
 #include <cassert>
+#include <list>
 
 /// ExprAST - Base class for all expression nodes.
 class ExprAST {
@@ -24,101 +25,65 @@ class NumberExprAST : public ExprAST {
 class VariableExprAST : public ExprAST {
     std::string Name;
 
- public:
- VariableExprAST(const std::string &Name) : Name(Name) {}
-};
-
-/// UnaryExprAST - Expression class for a unary operator.
-class UnaryExprAST : public ExprAST {
-    std::string Opcode;
-    std::unique_ptr<ExprAST> Operand;
-
- public:
-    UnaryExprAST(std::string Opcode, std::unique_ptr<ExprAST> Operand)
-     : Opcode(Opcode), Operand(std::move(Operand)) {}
+public:
+    VariableExprAST(const std::string &Name) : Name(Name) {}
 };
 
 /// BinaryExprAST - Expression class for a binary operator.
 class BinaryExprAST : public ExprAST {
     std::string Opcode;
-    std::unique_ptr<ExprAST> LHS, RHS;
+    std::shared_ptr<ExprAST> LHS, RHS;
 
 public:
-    BinaryExprAST(std::string Op, std::unique_ptr<ExprAST> LHS,
-                  std::unique_ptr<ExprAST> RHS)
+    BinaryExprAST(std::string Op, std::shared_ptr<ExprAST> LHS,
+                  std::shared_ptr<ExprAST> RHS)
         : Opcode(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
 };
 
-/// CallExprAST - Expression class for function calls.
-class CallExprAST : public ExprAST {
-    std::string Callee;
-    std::vector<std::unique_ptr<ExprAST>> Args;
+/// ReturnExprAST - Represents a value return expression
+class ReturnExprAST : public ExprAST {
+    std::shared_ptr<ExprAST> Expr;
 
- public:
- CallExprAST(const std::string &Callee,
-             std::vector<std::unique_ptr<ExprAST>> Args)
-     : Callee(Callee), Args(std::move(Args)) {}
+public:
+    ReturnExprAST(std::shared_ptr<ExprAST> expr)
+        : Expr(std::move(expr)) {}
 };
 
-/// IfExprAST - Expression class for if/then/else.
-class IfExprAST : public ExprAST {
-    std::unique_ptr<ExprAST> Cond, Then, Else;
+/// StatementListAST - Represents a list of statements executed in order
+class StatementListAST : public ExprAST {
+    std::list<std::shared_ptr<ExprAST>> Statements;
 
- public:
- IfExprAST(std::unique_ptr<ExprAST> Cond, std::unique_ptr<ExprAST> Then,
-           std::unique_ptr<ExprAST> Else)
-     : Cond(std::move(Cond)), Then(std::move(Then)), Else(std::move(Else)) {}
+public:
+    StatementListAST(){}
+    void Prepend(std::shared_ptr<ExprAST> t){
+        Statements.push_front(t);
+    }
 };
 
-/// ForExprAST - Expression class for for/in.
-class ForExprAST : public ExprAST {
-    std::string VarName;
-    std::unique_ptr<ExprAST> Start, End, Step, Body;
 
- public:
- ForExprAST(const std::string &VarName, std::unique_ptr<ExprAST> Start,
-            std::unique_ptr<ExprAST> End, std::unique_ptr<ExprAST> Step,
-            std::unique_ptr<ExprAST> Body)
-     : VarName(VarName), Start(std::move(Start)), End(std::move(End)),
-        Step(std::move(Step)), Body(std::move(Body)) {}
-};
 
 /// PrototypeAST - This class represents the "prototype" for a function,
 /// which captures its name, and its argument names (thus implicitly the number
-/// of arguments the function takes), as well as if it is an operator.
+/// of arguments the function takes).
 class PrototypeAST {
     std::string Name;
     std::vector<std::string> Args;
-    bool IsOperator;
-    unsigned Precedence; // Precedence if a binary op.
 
  public:
- PrototypeAST(const std::string &Name, std::vector<std::string> Args,
-              bool IsOperator = false, unsigned Prec = 0)
-     : Name(Name), Args(std::move(Args)), IsOperator(IsOperator),
-        Precedence(Prec) {}
+ PrototypeAST(const std::string &Name, std::vector<std::string> Args)
+     : Name(Name), Args(std::move(Args)) {}
     //Function *codegen();
     const std::string &getName() const { return Name; }
-
-    bool isUnaryOp() const { return IsOperator && Args.size() == 1; }
-    bool isBinaryOp() const { return IsOperator && Args.size() == 2; }
-
-    char getOperatorName() const {
-        assert(isUnaryOp() || isBinaryOp());
-        return Name[Name.size() - 1];
-    }
-
-    unsigned getBinaryPrecedence() const { return Precedence; }
 };
 
 /// FunctionAST - This class represents a function definition itself.
 class FunctionAST {
-    std::unique_ptr<PrototypeAST> Proto;
-    std::unique_ptr<ExprAST> Body;
+    std::shared_ptr<PrototypeAST> Proto;
+    std::shared_ptr<ExprAST> Body;
 
  public:
- FunctionAST(std::unique_ptr<PrototypeAST> Proto,
-             std::unique_ptr<ExprAST> Body)
+ FunctionAST(std::shared_ptr<PrototypeAST> Proto,
+             std::shared_ptr<ExprAST> Body)
      : Proto(std::move(Proto)), Body(std::move(Body)) {}
     //Function *codegen();
 };
